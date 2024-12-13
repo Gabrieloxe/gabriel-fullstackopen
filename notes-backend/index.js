405dbe8bc8af42c51e23074067d77577fd9cb34e
Gabriel Ong <gabrielongxe@gmail.com>
2024-12-13 18:01:04 +0800

    Update backend notes follow along application

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -1,128 +1,33 @@
-require('dotenv').config();
-// Express
+const config = require('./utils/config');
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+const notesRouter = require('./controllers/notes');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
 
-app.use(express.json());
-app.use(cors());
-app.use(express.static('build'));
+mongoose.set('strictQuery', false);
 
-// mongo DB connection
-const Note = require('./models/note');
+logger.info('connecting to', config.MONGODB_URI);
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method);
-  console.log('Path: ', request.path);
-  console.log('Body: ', request.body);
-  console.log('---');
-  next();
-};
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
-  }
-
-  next(error);
-};
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
 
 app.use(cors());
+app.use(express.static('dist'));
 app.use(express.json());
-app.use(requestLogger);
-app.use(express.static('build'));
+app.use(middleware.requestLogger);
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
-});
+app.use('/api/notes', notesRouter);
 
-app.get('/api/notes', async (request, response) => {
-  try {
-    const notes = await Note.find({});
-    response.json(notes);
-  } catch (e) {
-    response.status(400).send({ message: e.message, status: false });
-  }
-});
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-app.post('/api/notes', async (request, response, next) => {
-  try {
-    const body = request.body;
-    if (!body.content) {
-      return response.status(400).json({
-        error: 'content missing',
-      });
-    }
-
-    const newNote = new Note({
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-    });
-
-    const savedNote = await newNote.save();
-    response.json(savedNote);
-  } catch (e) {
-    next(e);
-  }
-});
-
-app.get('/api/notes/:id', async (request, response, next) => {
-  try {
-    const id = request.params.id;
-    const note = await Note.findById(id);
-    response.json(note);
-  } catch (e) {
-    next(e);
-  }
-});
-
-app.delete('/api/notes/:id', async (request, response, next) => {
-  try {
-    const id = request.params.id;
-    await Note.findByIdAndDelete(id);
-    response.status(204).end();
-  } catch (e) {
-    next(e);
-  }
-});
-
-app.put('/api/notes/:id', async (request, response, next) => {
-  try {
-    const body = request.body;
-    if (!body.content) {
-      return response.status(400).json({
-        error: 'content missing',
-      });
-    }
-    const id = request.params.id;
-    const noteUpdate = {
-      content: body.content,
-      date: new Date(),
-      important: body.important || false,
-    };
-    const options = { new: true };
-    const note = await Note.findOneAndReplace({ _id: id }, noteUpdate, options);
-    response.json(note);
-  } catch (e) {
-    next(e);
-  }
-});
-
-app.use(unknownEndpoint);
-app.use(errorHandler);
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-console.log(`Server running on port ${PORT}`);
+module.exports = app;
