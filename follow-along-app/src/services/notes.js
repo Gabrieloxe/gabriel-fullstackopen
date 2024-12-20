da49c416bcd11a3ca0e42b3da597889e0b957d4c
Gabriel Ong <gabrielongxe@gmail.com>
2024-12-20 11:42:57 +0800

    reconfigure notes frontend to vite build

--- follow-along-app/src/services/notes.js
+++ follow-along-app/src/services/notes.js
@@ -1,37 +1,23 @@
 import axios from 'axios';
-const baseURL = '/api/notes';
+const baseUrl = '/api/notes';
 
 const getAll = () => {
-  const request = axios.get(baseURL);
-  const nonExisting = {
-    id: 10000,
-    content: 'This note is not saved to server',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  }
-  return request.then(response => response.data.concat(nonExisting));
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
 };
 
-const create = (newObject) => {
-  const request = axios.post(baseURL, newObject);
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject);
   return request.then(response => response.data);
 };
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseURL}/${id}`, newObject);
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
   return request.then(response => response.data);
 };
 
-const remove = (id) =>{
-  const request = axios.delete(`${baseURL}/${id}`);
-  return request.then(response=> response.data);
-}
-
-const noteService = {
-    getAll,
-    create,
-    update,
-    remove
+export default {
+  getAll,
+  create,
+  update,
 };
-
-export default noteService;
--- notes-backend/app.js
+++ notes-backend/app.js
@@ -23,8 +23,8 @@ mongoose
     logger.error('error connecting to MongoDB:', error.message);
   });
 
-app.use(cors());
 app.use(express.static('dist'));
+app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
